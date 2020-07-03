1b9d4eaf7b007c24a7b52b6629b48b545037f20b
ernesto.sjunior <ernesto.sjunior@hotmail.com>
2020-07-03 11:41:11 -0300

    Update

--- database/migrations/2020_07_03_142848_create_people_table.php
+++ database/migrations/2020_07_03_142848_create_people_table.php
@@ -14,7 +14,7 @@ class CreatePeopleTable extends Migration
     public function up()
     {
         Schema::create('people', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('name');
             $table->string('cpfcnpj')->unique;
             $table->timestamps();
